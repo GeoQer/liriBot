2f8882c376c2144ac882682803bdb059185b9ecf
GeoQer <40548603+GeoQer@users.noreply.github.com>
2018-10-22 23:11:29 -0700

    minor updates. still not working

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+let Keys = require("./keys.js");
 
 let request = require("request");
 let fs = require("fs");
@@ -8,13 +9,15 @@ let userCommand = process.argv[2];
 let userSearch = process.argv.slice(3).join(" ");
 
 let Spotify = require('node-spotify-api');
-let spotify = new Spotify(keys.spotify);
+let spotify = new Spotify(Keys.spotify);
 
 
-function processInput(userCommand) {
+function processInput(userCommand, userSearch) {
     switch (userCommand) {
         case "concert-this":
             concertSearch(userSearch);
+            console.log(userCommand);
+            console.log(userSearch);
             break;
 
         case "spotify-this-song":
@@ -29,11 +32,14 @@ function processInput(userCommand) {
             doWhatSays();
             break;
 
-    }S
+    }
 
 }
 
-function concertSearch() {
+processInput();
+
+
+function concertSearch(userSearch) {
     let url = "https://rest.bandsintown.com/artists/" + userSearch + "/events?app_id=codingbootcamp"
 
     console.log(url);
@@ -49,6 +55,7 @@ function concertSearch() {
             console.log(JSON.parse(body))
 
         }
+        else console.log(error);
     })
 };
 
@@ -64,19 +71,19 @@ function songSearch() {
         });
 }
 
-function movieSearch(){
-    request("http://www.omdbapi.com/?t=" + process.argv.slice(3).join("+") + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
+function movieSearch() {
+    request("http://www.omdbapi.com/?t=" + process.argv.slice(3).join("+") + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
 
-  // If the request is successful (i.e. if the response status code is 200)
-  if (!error && response.statusCode === 200) {
+        // If the request is successful (i.e. if the response status code is 200)
+        if (!error && response.statusCode === 200) {
 
-    // Parse the body of the site and recover just the imdbRating
-    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-    console.log("Title: " + JSON.parse(body).title +
-        "\nPlot: " + JSON.parse(body).plot +
-    "\nRating: " + JSON.parse(body).rating);
-  }
-});
+            // Parse the body of the site and recover just the imdbRating
+            // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+            console.log("Title: " + JSON.parse(body).title +
+                "\nPlot: " + JSON.parse(body).plot +
+                "\nRating: " + JSON.parse(body).rating);
+        }
+    });
 }
 
 function doWhatSays() {
